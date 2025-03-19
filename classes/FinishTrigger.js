734b5e83e331c71e9fdd6a654fa2b2113e115602
TimeStepYT <herwig.orlich@gmail.com>
2025-03-19 21:27:45 +0100

    fix for real

--- classes/FinishTrigger.js
+++ classes/FinishTrigger.js
@@ -3,7 +3,7 @@ import Trigger from "./Trigger.js"
 export default class FinishTrigger extends Trigger {
     create(x, y, w, h) {
         super.create(x, y, w, h, (player) => {
-            player.scene.switchLevel(player.scene.levelNumber + 1)
+            player.layer.switchLevel(player.layer.levelNumber + 1)
         })
         return this
     }
